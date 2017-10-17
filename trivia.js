b76b3804c1b8b36fce1d3c0923e638c10e7c6d9c
Joshua Lawson <jacklegjosh@gmail.com>
2017-10-17 13:36:52 -0400

    refactored code to overwrite exisiting structure rather than append new elements, included an answer reveal on wrong answer, other small tweaks

--- trivia.js
+++ trivia.js
@@ -2,7 +2,16 @@ function init() {
     console.log("This is not Jeopardy!");
 
     let answerBlock = {};
-    let score = 0;
+    let counter = 0;
+
+    let body = document.querySelector('main');
+    let questionBox = document.querySelector('section');
+    let question = document.querySelector('h2');
+    let questionLine = document.querySelector('.clue');
+    let input = document.querySelector('input');
+    let submit = document.querySelector('button');
+    let score = document.querySelector('h3');
+    let answer = document.querySelector('.answer');
 
     function newQuestion() {
         let request = new XMLHttpRequest();
@@ -21,22 +30,11 @@ function init() {
     };
 
     function displayQuestion(info) {
-        let body = document.querySelector('main');
-        let questionBox = document.createElement('section');
-        body.appendChild(questionBox);
-        let question = document.createElement('h2');
+        console.log(question);
+        answer.classList.add('hidden');
+        score.textContent = `$` + counter;
         question.textContent = answerBlock.category.title;
-        let questionLine = document.createElement('p');
         questionLine.textContent = answerBlock.question;
-        questionBox.appendChild(question);
-        questionBox.appendChild(questionLine);
-        let input = document.createElement('input');
-        questionBox.appendChild(input);
-        input.placeholder = "Input answer here.";
-        let submit = document.createElement('button');
-        submit.textContent = "Submit";
-        questionBox.appendChild(submit);
-        submit.classList.add('submit');
         console.log("current value of answer: " + answerBlock.answer);
         submit.addEventListener('click', submitAnswer);
         //submit.classList.add('hidden');
@@ -44,41 +42,33 @@ function init() {
     };
 
     function submitAnswer() {
-        let input = document.querySelector('section:last-child input');
+        // let body = document.querySelector('section');
+        // let input = document.querySelector('input');
+        // let submit = document.querySelector('button');
         //let submission = input.value.toLowerCase();
         //console.log(submission);
         console.log('button working!');
         if (input.value != answerBlock.answer) {
             console.log('WRONG, dummy!');
-            let body = document.querySelector('section:last-child');
-            let submit = document.querySelector ('section:last-child button');
-            let input = document.querySelector('section:last-child input');
-            submit.classList.add('hidden');
-            input.classList.add('hidden');
-            let nay = document.createElement('h2');
-            nay.textContent = "Wrong!";
-            body.appendChild(nay);
-            newQuestion();
+            answer.textContent = `Wrong. The correct answer is: ` + answerBlock.answer;
+            answer.classList.remove('hidden');
+            // submit.classList.add('hidden');
+            // input.classList.add('hidden');
+            //let nay = document.createElement('h2');
+            //nay.textContent = "Wrong!";
+            //body.appendChild(nay);
+            setTimeout(newQuestion, 3500);
         } else {
             console.log("Yay!");
-            let body = document.querySelector('section:last-child');
-            let submit = document.querySelector ('section:last-child button');
-            let input = document.querySelector('section:last-child input');
-            submit.classList.add('hidden');
-            input.classList.add('hidden');
-            let yay = document.createElement('h2');
-            yay.textContent = "Correct!";
-            body.appendChild(yay);
             increaseScore();
             newQuestion();
         };
     };
 
     function increaseScore() {
-        let header = document.querySelector('h3');
-        console.log(score);
-        score += answerBlock.value;
-        header.textContent = `$` + score;
+        console.log(counter);
+        counter += answerBlock.value;
+        score.textContent = `$` + counter;
     }
 
     newQuestion();
